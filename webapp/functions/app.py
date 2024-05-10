891146f3b8ee60441827c10a6922c6f0d2ad801f
Gianluca DelBarba <gianlucadb06@gmail.com>
2024-05-09 21:49:24 -0400

    updated app.py

--- webapp/functions/app.py
+++ webapp/functions/app.py
@@ -1,8 +1,18 @@
 import sqlite3 
-import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     conn = sqlite3.connect('inventory.db')
+    cursor = conn.cursor()
+    cursor = conn.execute('SELECT * FROM inventory')
+    entries = cursor.fetchall()
+    conn.close()
+    return render_template('index.html', entries=entries)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
